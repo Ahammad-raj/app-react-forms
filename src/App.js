7d3a5364d22d93f8406f3203e42912c998315106
raj.ahamadbasha <raj.ahamadbasha@gmail.com>
2019-05-23 16:41:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import InputForm from './components/form';
+
+
+function App() {
+  return (
+    <div className="App">
+          <InputForm />
+    </div>
+  );
+}
+
+export default App;
